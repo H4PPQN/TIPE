c585e4bd7c937bf344d02b7fef01edda84a77c14
Amine Ait El Amiri <amine.aitelamiri2004@gmail.com>
2023-05-27 21:24:14 +0200

    last-commit

--- TIPE/functions.c
+++ TIPE/functions.c
@@ -54,33 +54,52 @@ void update_rame(graphe* g) {
 void reroute(graphe* g, station* from, station* to, station* affected_station) {
 
     int pathLength;
-    int* path = dijkstra(g, from, to, &pathLength);
+    int* shortest_path = dijkstra(g, from->id, to->id, &pathLength); //path: tableau qui contient le chemin optimal (chemin avec les IDs des stations etla station affectee aura un coeff tres eleve)
 
     int aff_station_id = affected_station->id;
 
-    for (int i = 0; i < NB_STATIONS_METRO; i++) { //mise a jour des tab_montees des stations du nouveau chemin
-        int ppl_to_reroute = (g->stations[i])->montee[aff_station_id];
-        (g->stations[i])->montee[aff_station_id] = 0;
-        for (int j = 1; j < pathLength - 1; j++) {
-            (g->stations[path[j - 1]])->montee[path[j]] += ppl_to_reroute;
+    //mise a jour des tab_montees des stations du nouveau chemin
+        int ppl_to_reroute = from->montee[aff_station_id]; //personnes qui souhaitaient monter pour aller a la stat_aff
+        from->montee[aff_station_id] = 0;
+        for (int j = 1; j < pathLength; j++) { //rajouter les personnes a transferer dans les stations du nouveau chemin
+            (g->stations[shortest_path[j - 1]])->montee[shortest_path[j]] += ppl_to_reroute;
         }
 
 
-    }
+
+   
 
 
     for (int i = 0; i < NB_RAMES_METRO; i++) { //mise a jour des tab_montees des stations des anciens chemins
         int ppl_to_reroute = (g->stations[i])->montee[aff_station_id];
         int pathCount;
         int length;
+        
         Path* allPaths = findAllPathsWrapper(g, from->id, to->id, &pathCount);
-        freePaths(allPaths, pathCount);
-        linked_stations(g, from->id, to->id, pathCount, &length);
 
-        for (int j = 0; j < length; j++) {
+        int* linked_stations = linked_stats(g, from->id, to->id, pathCount, &length);
+
+        for (int j = 0; j < pathCount; j++) {
+            
+            if (allPaths[j].nodes != shortest_path) {
+
+                for (int k = 0; k < allPaths[j].length; k++) {
+
+                    if ((g->stations[allPaths[j].nodes[k]])->montee[to->id] != 0) {
+                        for (int m = 1; m < allPaths[j].length; m++) {
+                            if ((g->stations[allPaths[j].nodes[k]])->montee[allPaths[j].nodes[m]] != 0) {
+                                (g->stations[allPaths[j].nodes[k]])->montee[allPaths[j].nodes[m]] -= (g->stations[allPaths[j].nodes[k]])->montee[to->id];
+                            }
+                          
+                        }
+                        ;
+                    }
+
+                    ;
+                }
 
 
-
+            }
 
        }
 
