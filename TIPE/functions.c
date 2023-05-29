c59932dc9ee7aa7382ae207f064132a6832b4f67
Amine Ait El Amiri <amine.aitelamiri2004@gmail.com>
2023-05-29 12:06:12 +0200

    last commit

--- TIPE/functions.c
+++ TIPE/functions.c
@@ -3,13 +3,72 @@
 #include <assert.h>
 #include "metro_v.h"
 #include "functions.h"
+#include <stdbool.h>
+
+
+station* station_create(int id, char* nom, int* correspondances, unsigned int capacity) {
+
+    station* new_station = malloc(sizeof(station));
+    new_station->id = id;
+    new_station->nom = nom;
+    new_station->correspondances = correspondances;
+    new_station->capacity= capacity;
+    new_station->current_people= 0;
+    new_station->in = 0;
+
+    new_station->montee= calloc(sizeof(int),NB_STATIONS_METRO);
+    return new_station;
+}
+
+rame* create_rame(int id, int depart, int arrivee, int* destinations) {
+
+    rame *new_rame = malloc(sizeof(rame));
+
+    new_rame->id = id;
+    new_rame->depart = depart;
+    new_rame->arrivee = arrivee;
+    new_rame->localisation= 0;
+
+    new_rame->current_people = 0;
+    new_rame->destinations = calloc(NB_STATIONS_METRO,sizeof(int));
+
+
+    return new_rame;
+
+}
+
+
+graphe* create_graph(station *stations,rame* rames, int** longueurs) {
+
+    graphe* new_graph = malloc(sizeof(graphe));
+    new_graph->longueurs = longueurs;
+    new_graph->rames = rames;
+    new_graph->stations = stations;
+    // creer la matrice vitesse
+    double** vitesse = (double**)malloc(NB_STATIONS_METRO * sizeof(double*));
+    for (int i = 0; i < NB_STATIONS_METRO; i++) {
+        vitesse[i] = (double*)malloc(NB_STATIONS_METRO * sizeof(double));
+    }
+
+    for (int i = 0; i < NB_STATIONS_METRO; i++) {
+        for (int j = 0; j < NB_STATIONS_METRO; j++) {
+            vitesse[i][j] = 0;
+        }
+    }
+
+
+    new_graph->vitesse = vitesse;
+
+
+    return new_graph;
+}
 
 void update_station(graphe* g) {
 
     for (int i = 0; i < NB_STATIONS_METRO; i++) {
         assert(((g->stations[i])->current_people + (g->stations[i])->in) < (g->stations[i])->capacity);
  
-        (g->stations[i])->current_people += (g->stations[i])->in;
+        (g->stations[i]).current_people += (g->stations[i]).in;
 
 
     }
@@ -23,85 +82,89 @@ void update_rame(graphe* g) {
 
     for (int i = 0; i < NB_RAMES_METRO; i++) {
 
-        int id_dep = (g->rames[i])->depart;
-        int id_arr = (g->rames[i])->arrivee;
-        int dist = g->longueur[id_arr][id_dep];
-        station *stat = g->stations[i];
-        if ((g->rames[i])->localisation + UPDATE_INTERV >= dist) {
-            int stat_arr = (g->stations[id_arr + 1])->id;
-            (g->rames[i])->depart = (g->stations[id_arr])->id;
-            (g->rames[i])->arrivee = stat_arr;
-            (g->rames[i])->localisation = 0;
-            (g->rames[i])->current_people += stat->montee[id_arr + 1] - ((g->rames[i])->destinations[id_arr]); //updating current_people (subtract people who got out and add those get in) 
-            (g->stations[i])->current_people -= stat->montee[id_arr + 1]; //soustraire ceux qui descendent (on ajoute rien car les personnes qui rentrent sont gerees par update_station)
-            stat->montee[id_arr + 1] = 0; //remise a zero du nombre de personnes qui souhaitaient monter dans la rame
+        int id_dep = (g->rames[i]).depart;
+        int id_arr = (g->rames[i]).arrivee;
+        int dist = g->longueurs[id_arr][id_dep];
+        station stat = g->stations[i];
+        if ((g->rames[i]).localisation + UPDATE_INTERV >= dist) {
+            int stat_arr = (g->stations[id_arr + 1]).id;
+            (g->rames[i]).depart = (g->stations[id_arr]).id;
+            (g->rames[i]).arrivee = stat_arr;
+            (g->rames[i]).localisation = 0;
+            (g->rames[i]).current_people += stat.montee[id_arr + 1] - ((g->rames[i]).destinations[id_arr]); //updating current_people (subtract people who got out and add those get in) 
+            (g->stations[i]).current_people -= stat.montee[id_arr + 1]; //soustraire ceux qui descendent (on ajoute rien car les personnes qui rentrent sont gerees par update_station)
+            stat.montee[id_arr + 1] = 0; //remise a zero du nombre de personnes qui souhaitaient monter dans la rame
 
-            (g->rames[i])->destinations[id_arr] = 0; //remise a zero des gens qui avaient pour destination
+            (g->rames[i]).destinations[id_arr] = 0; //remise a zero des gens qui avaient pour destination
 
         }
         else {
-            (g->rames[i])->localisation += (g->rames[i])->vitesse*UPDATE_INTERV;
+            (g->rames[i]).localisation += g->vitesse[id_dep][id_arr] * UPDATE_INTERV;
         }
 
 
     }
 }
 
+bool isAffStat_inPath(Path* allPaths, station* affected_station, int pathCount) {//verifie si le chemin contient la station affectee
 
+    for (int i = 0; i < pathCount; i++) {
+        for (int j = 0; j < allPaths[i].length; j++) {
 
+            if (allPaths[i].nodes[j] == affected_station->id) {
 
+                return true;
 
-void reroute(graphe* g, station* from, station* to, station* affected_station) {
+            }
+            else { return false; }
+        }
+    }
+};
 
-    int pathLength;
-    int* shortest_path = dijkstra(g, from->id, to->id, &pathLength); //path: tableau qui contient le chemin optimal (chemin avec les IDs des stations etla station affectee aura un coeff tres eleve)
+    void reroute(graphe * g, station * from, station * to, station * affected_station) {
+        
 
-    int aff_station_id = affected_station->id;
+        int pathLength;
+        int* shortest_path = dijkstra(g, from->id, to->id, &pathLength); //path: tableau qui contient le chemin optimal (chemin avec les IDs des stations etla station affectee aura un coeff tres eleve)
 
-    //mise a jour des tab_montees des stations du nouveau chemin
+        int aff_station_id = affected_station->id;
+
+        //mise a jour des tab_montees des stations du nouveau chemin
         int ppl_to_reroute = from->montee[aff_station_id]; //personnes qui souhaitaient monter pour aller a la stat_aff
         from->montee[aff_station_id] = 0;
         for (int j = 1; j < pathLength; j++) { //rajouter les personnes a transferer dans les stations du nouveau chemin
-            (g->stations[shortest_path[j - 1]])->montee[shortest_path[j]] += ppl_to_reroute;
+            (g->stations[shortest_path[j - 1]]).montee[shortest_path[j]] += ppl_to_reroute;
         }
-
-
-
-   
-
-
-    for (int i = 0; i < NB_RAMES_METRO; i++) { //mise a jour des tab_montees des stations des anciens chemins
-        int ppl_to_reroute = (g->stations[i])->montee[aff_station_id];
-        int pathCount;
-        int length;
         
-        Path* allPaths = findAllPathsWrapper(g, from->id, to->id, &pathCount);
 
-        int* linked_stations = linked_stats(g, from->id, to->id, pathCount, &length);
+        //mise a jour des tab_montees des stations des anciens chemins
+
+        int pathCount;
+
+
+        Path* allPaths = findAllPathsWrapper(g, from->id, to->id, &pathCount); //tous les chemins possibles 
 
         for (int j = 0; j < pathCount; j++) {
-            
-            if (allPaths[j].nodes != shortest_path) {
+
+            if (allPaths[j].nodes != shortest_path && isAffStat_inPath(allPaths, affected_station, pathCount)) {
+
 
                 for (int k = 0; k < allPaths[j].length; k++) {
 
-                    if ((g->stations[allPaths[j].nodes[k]])->montee[to->id] != 0) {
+                    if ((g->stations[allPaths[j].nodes[k]]).montee[to->id] != 0) {
                         for (int m = 1; m < allPaths[j].length; m++) {
-                            if ((g->stations[allPaths[j].nodes[k]])->montee[allPaths[j].nodes[m]] != 0) {
-                                (g->stations[allPaths[j].nodes[k]])->montee[allPaths[j].nodes[m]] -= (g->stations[allPaths[j].nodes[k]])->montee[to->id];
+                            if ((g->stations[allPaths[j].nodes[k]]).montee[allPaths[j].nodes[m]] != 0) {
+                                (g->stations[allPaths[j].nodes[k]]).montee[allPaths[j].nodes[m]] -= (g->stations[allPaths[j].nodes[k]]).montee[to->id];
                             }
-                          
+
                         }
-                        ;
+                        
                     }
 
-                    ;
+                    
                 }
 
 
             }
-
-       }
-
+        }
     }
-}
--- TIPE/metro_v.h
+++ TIPE/metro_v.h
@@ -3,17 +3,20 @@
 #define NB_LIGNES_METRO 14
 #define NB_RAMES_METRO 20
 #define UPDATE_INTERV 0.5
+#define Fick_const 10
+#define omega 5
 
 typedef struct Station
 {
 	int id;
-	char nom[50];
-	int nb_correspondances;
-	int* correspondances[NB_LIGNES_METRO];   //Tableau des ids des lignes accessibles depuis la station
+	char *nom;
+
+	int* correspondances;   //Tableau des ids des lignes accessibles depuis la station 
 	unsigned int capacity;                   //Nombre de personne maximum pouvant se trouver dans la station sans risques
 	unsigned int current_people;             //Nombre de personnes pr�sentes actuellement dans la station
 	unsigned int in;
-	int montee[NB_STATIONS_METRO];   //Nombre de personnes sortants de la sation � chaque actualisation
+	int *montee;   /* (taille  : [NB_STATIONS_METRO])Nombre de personnes souhaitant aller a chaque station->montee[0, 0, 0, 30, 16, 4, 3, 0, ..., 0] exemple tableau station 2
+										(30 personnes souhaitent descendre a la stat 3 ou plus loins,...,4 personnes souhaitent descendre a la stat 5 ou plus loins)*/
 } station;
 
 typedef struct Ligne
@@ -30,25 +33,25 @@ typedef struct Rame
 	int depart;                         //Station de d�part du tron�on actuel
 	int arrivee;                        //Station d'arriv�e du tron�on actuel
 	double localisation;                        //Portion du tron�on parcouru (nombre d'actualisation depuis que la rame est sur le tron�on)
-	double vitesse;
+	//double vitesse;
 	int current_people;
-	int destinations[NB_STATIONS_METRO]; //nombre des gens et leur destination en fonction de la ligne
+	int *destinations; //(taille: [NB_STATIONS_METRO])nombre des gens et leur destination en fonction de la ligne
 } rame;
 
 typedef struct Graphe
 {
-	station* stations[NB_STATIONS_METRO];    //Tableau de toutes les stations
-	ligne* lignes[NB_LIGNES_METRO];          //Tableau de toutes les lignes
-	rame* rames[NB_RAMES_METRO];             //Tableau de toutes les rames
-	int longueur[NB_STATIONS_METRO][NB_STATIONS_METRO];
-	int vitesse[NB_STATIONS_METRO][NB_STATIONS_METRO];
+	station* stations;    //(taille: [NB_STATIONS_METRO])Tableau de toutes les stations
+	/*ligne* lignes[NB_LIGNES_METRO]; */         //Tableau de toutes les lignes
+	rame* rames;             //(taille : [NB_RAMES_METRO])Tableau de toutes les rames
+	int** longueurs;							//taille : [NB_STATIONS_METRO][NB_STATIONS_METRO]
+	double **vitesse;					    	//taille : [NB_STATIONS_METRO][NB_STATIONS_METRO]
 } graphe;
 
 //constructeurs
-station* creer_station(int id, char* nom);
+station* station_create(int id, char* nom, int*correspondance,unsigned int capacity);
 ligne* creer_ligne(int id, char* nom);
-rame* creer_rame(int id);
-graphe* creer_graphe_vide();
+rame* create_rame(int id);
+graphe* create_graph();
 
 //fonction pour faire �voluer le graphe dans le temps
 void update_station(graphe* g);
